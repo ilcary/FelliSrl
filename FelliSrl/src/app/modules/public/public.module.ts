cc30f543a21d370053b73be8e1eef7acf23753c9
ilCary <carisiodigiampietro54@gmail.com>
2023-12-17 14:23:37 +0100

    restyling homepage and route selection

--- FelliSrl/src/app/modules/public/public.module.ts
+++ FelliSrl/src/app/modules/public/public.module.ts
@@ -4,12 +4,20 @@ import {HomepageComponent} from "./homepage/homepage.component";
 import {SharedModule} from "../shared/shared.module";
 import {RouterModule} from "@angular/router";
 import {PublicModuleRoutes} from "./public.routes";
+import { AboutUsComponent } from './about-us/about-us.component';
+import { WorkWithUsComponent } from './work-with-us/work-with-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    AboutUsComponent,
+    WorkWithUsComponent,
+    ContactsComponent,
+    GalleryComponent
   ],
   imports: [
     SharedModule,
--- FelliSrl/src/app/modules/public/public.routes.ts
+++ FelliSrl/src/app/modules/public/public.routes.ts
@@ -1,5 +1,9 @@
 import {Routes} from "@angular/router";
 import {HomepageComponent} from "./homepage/homepage.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {WorkWithUsComponent} from "./work-with-us/work-with-us.component";
+import {ContactsComponent} from "./contacts/contacts.component";
 
 export const PublicModuleRoutes: Routes = [
   {
@@ -7,7 +11,23 @@ export const PublicModuleRoutes: Routes = [
     component: HomepageComponent,
   },
   {
-    path:'',
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'chi-siamo',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'lavora-con-noi',
+    component: WorkWithUsComponent,
+  },
+  {
+    path: 'contatti',
+    component: ContactsComponent,
+  },
+  {
+    path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
